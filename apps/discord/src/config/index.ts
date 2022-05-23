f1792732810bb023158c9b38006bf8e010a4bb9f
benno <bennokohrs@gmail.com>
2022-05-23 12:12:36 +0200

    wip

--- apps/discord/src/config/index.ts
+++ apps/discord/src/config/index.ts
@@ -2,15 +2,15 @@ import dotenv from 'dotenv';
 
 // Calling before loading the configurations to load the environment variables first
 const ENVIRONMENT = process.env.NODE_ENV || 'local'; // https://stackoverflow.com/questions/11104028/why-is-process-env-node-env-undefined
-dotenv.config({ path: `.env.${ENVIRONMENT}` });
+dotenv.config({path: `.env.${ENVIRONMENT}`});
 
 // Configuration Objects
 import discordConfig from './discord.config';
 import solanaConfig from './solana.config';
 
 export const config = {
-  discord: discordConfig,
-  sol: solanaConfig,
+    discord: discordConfig,
+    sol: solanaConfig,
 };
 
 console.log(`Loaded Environment Variables from '.env.${ENVIRONMENT}'`, config);
